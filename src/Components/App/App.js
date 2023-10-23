518361d70e3f0791cc038512370c326343b5f005
Your Name <your_email@address>
2023-10-23 16:17:39 +0100

    MUI Icons installed, Accordion Component created

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import Container from "@mui/material/Container";
+import BasicAccordion from "../Accordion/Accordion.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <Container maxWidth="sm">
+        <BasicAccordion />
+      </Container>
     </div>
   );
 }
