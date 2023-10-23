43aeb5051a926b3c07652d26a417932f89c12dae
Your Name <your_email@address>
2023-10-23 16:31:35 +0100

    Card Component implemented

--- src/Components/Accordion/Accordion.js
+++ src/Components/Accordion/Accordion.js
@@ -3,6 +3,7 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ProjectCard from '../Card/Card';
 
 export default function BasicAccordian() {
   return (
@@ -20,6 +21,8 @@ export default function BasicAccordian() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             malesuada lacus ex, sit amet blandit leo lobortis eget.
           </Typography>
+          <p>Hello World!</p>
+          <img src='https://images.unsplash.com/photo-1697618365363-1f4638b9c7d5?auto=format&fit=crop&q=80&w=1032&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'height={400} width={400} alt='colourful lines'/>
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -31,10 +34,7 @@ export default function BasicAccordian() {
           <Typography>Accordion 2</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          <ProjectCard />
         </AccordionDetails>
       </Accordion>
       <Accordion disabled>
