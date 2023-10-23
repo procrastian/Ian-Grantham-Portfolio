661bab0f26355cdd528d556d2a40a1ebb181a75f
Your Name <your_email@address>
2023-10-23 16:36:12 +0100

    Box Component Added

--- src/Components/Accordion/Accordion.js
+++ src/Components/Accordion/Accordion.js
@@ -1,9 +1,10 @@
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ProjectCard from '../Card/Card';
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ProjectCard from "../Card/Card";
+import BoxSx from "../Box/Box";
 
 export default function BasicAccordian() {
   return (
@@ -14,7 +15,7 @@ export default function BasicAccordian() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography>Accordion 1 - Plain</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -22,7 +23,12 @@ export default function BasicAccordian() {
             malesuada lacus ex, sit amet blandit leo lobortis eget.
           </Typography>
           <p>Hello World!</p>
-          <img src='https://images.unsplash.com/photo-1697618365363-1f4638b9c7d5?auto=format&fit=crop&q=80&w=1032&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'height={400} width={400} alt='colourful lines'/>
+          <img
+            src="https://images.unsplash.com/photo-1697618365363-1f4638b9c7d5?auto=format&fit=crop&q=80&w=1032&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            height={400}
+            width={400}
+            alt="colourful lines"
+          />
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -31,21 +37,33 @@ export default function BasicAccordian() {
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
-          <Typography>Accordion 2</Typography>
+          <Typography>Accordion 2 - Card</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <ProjectCard />
         </AccordionDetails>
       </Accordion>
-      <Accordion disabled>
+      <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel3a-content"
           id="panel3a-header"
         >
+          <Typography>Accordion 3 - Box</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <BoxSx />
+        </AccordionDetails>
+      </Accordion>
+      <Accordion disabled>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4a-content"
+          id="panel4a-header"
+        >
           <Typography>Disabled Accordion</Typography>
         </AccordionSummary>
       </Accordion>
     </div>
-  )
+  );
 }
