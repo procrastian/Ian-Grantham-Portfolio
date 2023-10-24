efbb0630922993911ce68ab96f3db7734982a387
Your Name <your_email@address>
2023-10-24 16:51:45 +0100

    theme added to App Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,15 +2,24 @@ import Container from "@mui/material/Container";
 import BasicAccordion from "../Accordion/Accordion.js";
 import "./App.css";
 import ButtonAppBar from "../AppBar/AppBar.js";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    // mode: "dark"
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <ButtonAppBar></ButtonAppBar>
-      <Container maxWidth="sm">
-        <BasicAccordion />
-      </Container>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <ButtonAppBar></ButtonAppBar>
+        <Container maxWidth="sm">
+          <BasicAccordion />
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
