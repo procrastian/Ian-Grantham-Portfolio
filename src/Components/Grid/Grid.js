32fb3a944463ce6607f334682a20c2994290f690
Your Name <your_email@address>
2023-10-24 15:21:35 +0100

    github icon added with link for accordion grid

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -1,15 +1,16 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import ButtonBase from '@mui/material/ButtonBase';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import ButtonBase from "@mui/material/ButtonBase";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
-const Img = styled('img')({
-  margin: 'auto',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
+const Img = styled("img")({
+  margin: "auto",
+  display: "block",
+  maxWidth: "100%",
+  maxHeight: "100%",
 });
 
 export default function ProjectGrid() {
@@ -17,17 +18,20 @@ export default function ProjectGrid() {
     <Paper
       sx={{
         p: 2,
-        margin: 'auto',
+        margin: "auto",
         maxWidth: 500,
         flexGrow: 1,
         backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+          theme.palette.mode === "dark" ? "#1A2027" : "#fff",
       }}
     >
       <Grid container spacing={2}>
         <Grid item>
           <ButtonBase sx={{ width: 128, height: 128 }}>
-            <Img alt="complex" src="https://images.unsplash.com/photo-1518780664697-55e3ad937233?auto=format&fit=crop&q=80&w=1065&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <Img
+              alt="complex"
+              src="https://images.unsplash.com/photo-1518780664697-55e3ad937233?auto=format&fit=crop&q=80&w=1065&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
           </ButtonBase>
         </Grid>
         <Grid item xs={12} sm container>
@@ -44,9 +48,18 @@ export default function ProjectGrid() {
               </Typography>
             </Grid>
             <Grid item>
-              <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                Remove
-              </Typography>
+              <Typography
+                sx={{ cursor: "pointer" }}
+                variant="body2"
+              ></Typography>
+            </Grid>
+            <Grid item>
+              <a
+                href="https://github.com/procrastian/react-media-prompts"
+                target="blank"
+              >
+                <GitHubIcon></GitHubIcon>
+              </a>
             </Grid>
           </Grid>
           <Grid item>
