19edfca091f330b08b7654f309d811d404099cc5
Ian Grantham <dev.iangrantham@gmail.com>
2023-11-20 14:52:39 +0000

    components moved in front of background effect

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,18 @@
 import Container from "@mui/material/Container";
 import BasicAccordion from "../Accordion/Accordion.js";
 import "./App.css";
-import ButtonAppBar from "../AppBar/AppBar.js";
+// import ButtonAppBar from "../AppBar/AppBar.js";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Background from "../Background/Background.js";
+import { Box } from "@mui/material";
 
 const theme = createTheme({
   palette: {
     // mode: "dark"
+  },
+  zIndex: {
+    pattern: 1050,
+    content: 1000,
   },
 });
 
@@ -15,9 +20,16 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Background />
-        <ButtonAppBar></ButtonAppBar>
-        <Container maxWidth="sm">
+        <Background zIndex sx={{zIndex: "pattern", position: 'absolute'}}>
+
+        </Background>
+        {/* <ButtonAppBar></ButtonAppBar> */}
+        <Container
+          maxWidth="sm"
+          className="content"
+          zIndex
+          sx={{ zIndex: "content", position: "absolute", display: "flex" }}
+        >
           <BasicAccordion />
         </Container>
       </div>
