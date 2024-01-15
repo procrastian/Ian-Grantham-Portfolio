9e12dbef097f94f37c83e334f9e702ab8285e950
Your Name <your_email@address>
2024-01-15 16:36:04 +0000

    circuit board pattern now tiles and resizes for whole screen

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,8 @@
-import Container from "@mui/material/Container";
-import BasicAccordion from "../Accordion/Accordion.js";
+// import Container from "@mui/material/Container";
+// import BasicAccordion from "../Accordion/Accordion.js";
 import "./App.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import Background from "../Background/Background.js";
+import BackgroundImage from "../Background/Background.js";
 
 const theme = createTheme({
   palette: {
@@ -18,17 +18,16 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Background zIndex sx={{zIndex: "pattern", position: 'absolute'}}>
-
-        </Background>
-        <Container
+        {/* <BackgroundImage/> */}
+        <div class="el"></div>
+        {/* <Container
           maxWidth="sm"
           className="content"
           zIndex
           sx={{ zIndex: "content", position: "absolute", display: "flex" }}
-        >
-          <BasicAccordion />
-        </Container>
+        > */}
+          {/* <BasicAccordion /> */}
+        {/* </Container> */}
       </div>
     </ThemeProvider>
   );
