b531dc3fd5a1ee2aa1ec107af1e5806fb49750a3
Your Name <your_email@address>
2024-01-16 16:58:57 +0000

    scalable mask image applied

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,5 @@
-// import Container from "@mui/material/Container";
-// import BasicAccordion from "../Accordion/Accordion.js";
 import "./App.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import BackgroundImage from "../Background/Background.js";
 
 const theme = createTheme({
   palette: {
@@ -18,16 +15,8 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        {/* <BackgroundImage/> */}
-        <div class="el"></div>
-        {/* <Container
-          maxWidth="sm"
-          className="content"
-          zIndex
-          sx={{ zIndex: "content", position: "absolute", display: "flex" }}
-        > */}
-          {/* <BasicAccordion /> */}
-        {/* </Container> */}
+        <div className="blackoutLayer"></div>
+      
       </div>
     </ThemeProvider>
   );
